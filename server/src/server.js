c7019aa24bbd396b09a57695a1a96736674564bb
vivek-joshi26 <vivek.jsh2@gmail.com>
2023-05-14 11:49:57 -0700

    Webcam Combining Videos Fix

--- server/src/server.js
+++ server/src/server.js
@@ -5,8 +5,10 @@ const mongoose = require("mongoose")
 require("dotenv").config({path : "./config.env"})
 var bodyParser = require("body-parser");
 var http = require("http");
-var OpenVidu = require("openvidu-node-client").OpenVidu;
 var UserRouter = require("./routers/UserRouter")
+
+const path = require('path');
+const { spawn } = require('child_process');
 
 
 const multer = require('multer');
@@ -89,79 +91,240 @@ app.post('/upload', upload.single('file'), function(req, res) {
 
 app.use("/users", UserRouter);
 
+// TESTING FOR UPLOADING CHUNKS FROM WEBCAM, created the chunks folder for the same
 
 
-// Open Vidu Basic node implementation for connection to a web conference
+const storage1 = multer.diskStorage({
+  destination: function(req, file, cb) {
+    cb(null, 'chunks');
+  },
+  filename: function(req, file, cb) {
+    cb(null, file.originalname);
+  }
+});
+
+
+const upload1 = multer({ storage: storage1 });
+
+
+app.post('/uploadChunks', upload1.single('file'), function(req, res) {
+
+  const baseDirectory = './chunks'
+  const filename = req.file.filename;
+  console.log(filename);
+
+  const inputFile = `./chunks/${req.file.filename}`;
+  // const outputFile = `./chunks/${req.file.filename.replace(/\.[^/.]+$/, '')}.mp4`;
+
+  if (filename) {
+    const videoName = filename.split(".")[0];
+    console.log('Video name:', videoName);
+
+    const individualDetails = videoName.split("_");
+    const userid = individualDetails[0];
+    const camera = individualDetails[1];
+    const startTime = individualDetails[2];
+    const chunkTime = individualDetails[3];
+
+    const directoryPath = path.join(baseDirectory, `${userid}_${camera}_${startTime}`);
+    const outputFile = `./chunks/${userid}_${camera}_${startTime}/` + chunkTime  + ".mp4"
+
+    // Check if directory exists, create it if it doesn't
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true });
+    }
+
+    console.log('Input file: ' , inputFile)
+    console.log('Output file: ' , outputFile)
+   
+
+    ffmpeg(inputFile)
+    .output(outputFile)
+    .on('end', function() {
+      console.log('Video conversion complete');
+      fs.unlink(inputFile, function (err) {
+          if (err) {
+              console.log('Error deleting input file:', err);
+          } else {
+              console.log('Input file deleted successfully');
+          }})
+      res.status(200).send('Video uploaded and converted successfully');
+    })
+    .run();
+
+  } else {
+    console.log('Filename is undefined or null');
+    res.sendStatus(400);
+  }
+});
+
+    // Example usage in your code
+    const baseDirectory = path.resolve('./chunks'); // Resolve to absolute path
+
+app.post('/stopChunks', upload1.single('file'), function(req, res) {
+
+  const baseDirectory = './chunks'
+  const filename = req.file.filename;
+  console.log(filename);
+
+  const inputFile = `./chunks/${req.file.filename}`;
+  // const outputFile = `./chunks/${req.file.filename.replace(/\.[^/.]+$/, '')}.mp4`;
+
+  if (filename) {
+    const videoName = filename.split(".")[0];
+    console.log('Video name:', videoName);
+
+    const individualDetails = videoName.split("_");
+    const userid = individualDetails[0];
+    const camera = individualDetails[1];
+    const startTime = individualDetails[2];
+    const chunkTime = individualDetails[3];
+
+    const directoryPath = path.join(baseDirectory, `${userid}_${camera}_${startTime}`);
+    const outputFile = `./chunks/${userid}_${camera}_${startTime}/` + chunkTime  + ".mp4"
+    // const directoryPath1 = `./chunks/${userid}_${camera}_${startTime}/`
+
+    // const outputFile1 = `./chunks/${userid}_${camera}_${startTime}/` + 'combine.mp4'
 
 
 
-// console.warn('Application server connecting to OpenVidu at ' + process.env.OPENVIDU_URL);
+    const directoryPath1 = path.join(baseDirectory, `${userid}_${camera}_${startTime}`);
+    const outputFile1 = path.join(directoryPath1, videoName + '.mp4');
 
-// var openvidu = new OpenVidu(
-//   process.env.OPENVIDU_URL,
-//   process.env.OPENVIDU_SECRET
-// );
-
-// app.post("/api/sessions", async (req, res) => {
-//   console.log('LINE 63')
-//   var session = await openvidu.createSession(req.body);
-//   res.send(session.sessionId);
-// });
-
-// app.post("/api/sessions/:sessionId/connections", async (req, res) => {
-//   var session = openvidu.activeSessions.find(
-//     (s) => s.sessionId === req.params.sessionId
-//   );
-//   if (!session) {
-//     console.log('LINE 72')
-//     res.status(404).send();
-//   } else {
-//     console.log('LINE 75')
-//     var connection = await session.createConnection(req.body);
-//     res.send(connection.token);
-//   }
-// });
+    // combineAllMP4Files(directoryPath1, outputFile1)
 
 
-// // how to create session to publish IP cameras
-// // 1st need to create a session object using app.post("/api/sessions", async (req, res)
-// // then using that sessionId need to register the IP camera URI
+    // Check if directory exists, create it if it doesn't
+    if (!fs.existsSync(directoryPath)) {
+      fs.mkdirSync(directoryPath, { recursive: true });
+    }
+
+    console.log('Input file: ' , inputFile)
+    console.log('Output file: ' , outputFile)
+   
+
+    ffmpeg(inputFile)
+    .output(outputFile)
+    .on('end', function() {
+      console.log('Video conversion complete');
+      fs.unlink(inputFile, function (err) {
+          if (err) {
+              console.log('Error deleting input file:', err);
+          } else {
+              console.log('Input file deleted successfully');
+          }})
+      combineAllMP4Files(directoryPath1, outputFile1)
+      res.status(200).send('Video uploaded and converted successfully');
+    })
+    .run();
 
 
-// var connectionProperties = {
-//   type: "IPCAM",
-//   rtspUri: "rtsp://your.camera.ip:7777/path",
-//   adaptativeBitrate: true,
-//   onlyPlayWithSubscribers: false,
-//   networkCache: 1000
-// };
-
-
-
-// // this API should return the stream created for that IP camera,
-// // that can be accessed on the UI using Subscribe from other user's stream - https://docs.openvidu.io/en/stable/cheatsheet/subscribe-unsubscribe/
-
-// app.post("/api/sessions/:sessionId/ipcamera", async (req, res) => {
-//   var session = openvidu.activeSessions.find(
-//     (s) => s.sessionId === req.params.sessionId
-//   );
-//   if (!session) {
-//     res.status(404).send();
-//   } else {
-//     var connection = await session.createConnection(connectionProperties);
-//     res.send(connection);
-//   }
-// });
+    
 
 
 
 
+  } else {
+    console.log('Filename is undefined or null');
+    res.sendStatus(400);
+  }
+
+});
+
+
+function combineAllMP4Files(directoryPath, outputFilePath) {
+  const inputFiles = fs.readdirSync(directoryPath)
+  .filter(file => path.extname(file) === '.mp4')
+  .map(file => path.join(directoryPath, file));
+
+  const ffmpegCommand = ffmpeg({ source: inputFiles[0] });
+  for (let i = 1; i < inputFiles.length; i++) {
+    ffmpegCommand.input(inputFiles[i]);
+  }
+
+  console.log('Directory Path:', directoryPath)
+  console.log('Input files:', inputFiles)
+
+  ffmpegCommand
+    .on('end', () => {
+      console.log("Merge is complete")
+      // Move the file to the upload folder
+      const destinationPath = path.join('./uploads', path.basename(outputFilePath));
+      fs.renameSync(outputFilePath, destinationPath);
+      console.log('File moved to:', destinationPath);
+
+      // Delete the original directory and its contents
+      fs.rmdirSync(directoryPath, { recursive: true });
+      console.log('Directory deleted:', directoryPath);
+    })
+    .on('error', (err) => console.log("Error:", err))
+    .mergeToFile(outputFilePath);
+}
+
+
+function checkRemainingVideos(){
+  const chunksDirectory = './chunks';
+
+  // Read the contents of the chunks directory
+  fs.readdir(chunksDirectory, (err, files) => {
+    if (err) {
+      console.error('Error reading directory:', err);
+      return;
+    }
+
+    // Filter out directories
+    const directories = files.filter(file => {
+      const fullPath = path.join(chunksDirectory, file);
+      return fs.statSync(fullPath).isDirectory();
+    });
+
+    console.log('Directories:', directories);
+
+    directories.forEach(directory => combineAllMP4FilesStartUp(chunksDirectory + "/" + directory))
+  });
+}
+
+function combineAllMP4FilesStartUp(directoryPath) {
+  const inputFiles = fs.readdirSync(directoryPath)
+  .filter(file => path.extname(file) === '.mp4')
+  .map(file => path.join(directoryPath, file));
+
+  const ffmpegCommand = ffmpeg({ source: inputFiles[0] });
+  for (let i = 1; i < inputFiles.length; i++) {
+    ffmpegCommand.input(inputFiles[i]);
+  }
+
+  console.log('Directory Path:', directoryPath)
+  console.log('Input files:', inputFiles)
+
+  const outputFileArray = inputFiles[inputFiles.length - 1].split('/')
+  const outputFile = outputFileArray[1] + "_" + outputFileArray[2]
+  const outputFilePath = directoryPath + "/" + outputFile
+  console.log('Output file:', outputFile)
+  console.log('Output filePath:', outputFilePath)
+
+  ffmpegCommand
+    .on('end', () => {
+      console.log("Merge is complete")
+      // Move the file to the upload folder
+      const destinationPath = path.join('./uploads', path.basename(outputFilePath));
+      fs.renameSync(outputFilePath, destinationPath);
+      console.log('File moved to:', destinationPath);
+
+      // Delete the original directory and its contents
+      fs.rmdirSync(directoryPath, { recursive: true });
+      console.log('Directory deleted:', directoryPath);
+    })
+    .on('error', (err) => console.log("Error:", err))
+    .mergeToFile(outputFilePath);
+}
 
 
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`)
     // Call the checkForNewVideos function when the Node.js application starts up
    // checkForNewVideos();
+   checkRemainingVideos();
 })
 
 
